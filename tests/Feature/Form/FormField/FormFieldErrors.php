a590c6483550c2a7e46c75293d6e4a9aa472d803
Patrick Robrecht <patrick.robrecht@portavice.de>
2023-12-30 19:43:32 +0100

    Extend tests to check <select> and <textarea> as well

--- tests/Feature/Form/FormField/FormFieldErrors.php
+++ tests/Feature/Form/FormField/FormFieldErrors.php
@@ -25,13 +25,16 @@ class FormFieldErrors extends ComponentTestCase
         );
     }
 
-    public function testFormFieldWithInputGroupShowsErrors(): void
+    /**
+     * @dataProvider types
+     */
+    public function testFormFieldWithInputGroupShowsErrors(string $type, string $output): void
     {
         $this->assertBladeRendersToHtml(
             '<div class="mb-3">
                 <label for="test" class="form-label">Test</label>
                 <div class="input-group has-validation">
-                    <input id="test" name="test" type="text" value="" class="form-control is-invalid"/>
+                    ' . $output . '
                     <label for="test" class="input-group-text">Append</label>
                     <div role="alert" class="invalid-feedback">
                         The test field is required.
@@ -39,7 +42,7 @@ class FormFieldErrors extends ComponentTestCase
                 </div>
             </div>',
             $this->bladeView(
-                '<x-bs::form.field name="test" type="text" value="">
+                '<x-bs::form.field name="test" ' . $type . ' value="">
                     Test
                     <x-slot:appendText>Append</x-slot:appendText>
                 </x-bs::form.field>',
@@ -48,5 +51,26 @@ class FormFieldErrors extends ComponentTestCase
                 ],
             )
         );
+    }
+
+    public static function types(): array
+    {
+        return [
+            [
+                'type="select" :options="[1 => 1, 2 => 2]"',
+                '<select id="test" name="test" class="form-select is-invalid">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                </select>',
+            ],
+            [
+                'type="text"',
+                '<input id="test" name="test" type="text" value="" class="form-control is-invalid"/>',
+            ],
+            [
+                'type="textarea"',
+                '<textarea id="test" name="test" class="form-control is-invalid"></textarea>',
+            ],
+        ];
     }
 }
